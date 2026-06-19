17da8afd00b0e822cf4c014d3b928dfa5a88ded1
nizamudheen <nizamudheents.ae@gmail.com>
2026-06-20 00:31:43 +0400

    Day 63 - Express middleware

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log("Middleware Running");
+    next();
+});
+
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
