dca1e17a0d250a339cd08650c62381e5fbf8c5f8
nizamudheen <nizamudheents.ae@gmail.com>
2026-06-21 18:58:10 +0400

    Day 67 - Create GET API with JSON response

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -3,8 +3,15 @@ const express = require("express");
 
 const router = express.Router();
 
+const customers = [
+  { id: 1, name: "Nizam", city: "Dubai" },
+  { id: 2, name: "Ali", city: "Abu Dhabi" },
+  { id: 3, name: "Hassan", city: "Sharjah" }
+];
+
+
 router.get("/", (req, res) => {
-    res.send("All Customers");
+    res.json(customers);
 });
 
 router.get("/:id", (req, res) => {
