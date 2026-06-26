3f92e1d3fdf0ff97769c5ece2b343121f36a896c
nizamudheen <nizamudheents.ae@gmail.com>
2026-06-26 20:46:33 +0400

    Day 79-80 - Connect Express application to MongoDB

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { client, dbName } = require("./db/connection");
 
 const app = express();
 
@@ -8,6 +9,21 @@ const customerRoutes = require("./routes/customerRoutes");
 
 app.use("/customers", customerRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+// DB CONNECT
+client.connect()
+.then(() => {
+
+    console.log("✅ Connected to MongoDB");
+
+    const db = client.db(dbName);
+
+    console.log(db.databaseName);
+
+    app.listen(5000, () => {
+        console.log("Server running on port 5000");
+    });
+
+})
+.catch((err) => {
+    console.log(err);
 });
