10226877cc8b19e70620f4f9fc5aff23deb5f159
nizamudheen <nizamudheents.ae@gmail.com>
2026-06-28 12:38:52 +0400

    Day 85 - Delete customer using MongoDB

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -148,26 +148,53 @@ router.put("/:id", async (req, res) => {
    })
 })
 
-router.delete("/:id", (req, res) => {
 
-  const customerIndex = customers.findIndex(
-    (c) => c.id === parseInt(req.params.id)
-  );
+// DELETE OR REMOVE DAT / CUSTOMER
 
-  if (customerIndex === -1) {
-    return res.status(404).json({
-      message: "Customer not found"
+router.delete("/:id", async (req, res) => {
+    const id = new ObjectId(req.params.id);
+
+    
+    const customer = await req.customersCollection.findOne({
+      _id: id
     });
-  }
+    
+    
+    if(!customer) {
+      return res.status(404).json({
+        message: "Customer not found"
+      });
+    }
+    
+    
+    await req.customersCollection.deleteOne({
+      _id: id
+    })
+    
+    
+    res.json({
+      message: "Csutomer Deleted Successfully"
+      
+    })
 
-  const deletedCustomer = customers[customerIndex];
 
-  customers.splice(customerIndex, 1);
+    // const customerIndex = customers.findIndex(
+    //   (c) => c.id === parseInt(req.params.id)
+    // );
+  // if (customerIndex === -1) {
+  //   return res.status(404).json({
+  //     message: "Customer not found"
+  //   });
+  // }
 
-  res.json({
-    message: "Customer deleted successfully",
-    customer: deletedCustomer
-  });
+  // const deletedCustomer = customers[customerIndex];
+
+  // customers.splice(customerIndex, 1);
+
+  // res.json({
+  //   message: "Customer deleted successfully",
+  //   customer: deletedCustomer
+  // });
 
 });
 
